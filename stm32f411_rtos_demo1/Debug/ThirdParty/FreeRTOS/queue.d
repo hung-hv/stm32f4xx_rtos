3fd4042f79781a66e8064d22b072f2aca4e25832
hung-hv <viethung170230@gmail.com>
2023-12-28 02:38:52 +0700

    adding free rtos to stm32 hal

---
+++ stm32f411_rtos_demo1/Debug/ThirdParty/FreeRTOS/queue.d
@@ -0,0 +1,23 @@
+ThirdParty/FreeRTOS/queue.o: ../ThirdParty/FreeRTOS/queue.c \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/queue.h \
+ C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/task.h
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/queue.h:
+C:/Users/vieth/Documents/stm32/FreeRTOS/STM32RTOS_workspace/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/task.h:
